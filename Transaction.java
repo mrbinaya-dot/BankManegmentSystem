fa591f7f28342b8bbc51d4784f4617868f9f6833
mrbinaya-dot <mrnay166@gmail.com>
2025-08-24 15:51:44 +0545

    adding all the buttons in transaction

--- Transaction.java
+++ Transaction.java
@@ -1,44 +1,94 @@
-import javax.swing.*;
+
+
 import java.awt.*;
-
+import java.awt.event.*;
 import javax.swing.*;
-import java.awt.*;
+import java.sql.*;
 
-public class Transaction extends JFrame {
+public class Transaction extends JFrame implements ActionListener{
 
-    Transaction() {
-        // Load image
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm3.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(700, 900, Image.SCALE_DEFAULT);
+    JLabel l1;
+    JButton b1,b2,b3,b4,b5,b6,b7;
+    String pin;
+    Transaction(){
 
-        // Wrap scaled image back in ImageIcon
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 900, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel j1 = new JLabel(i3);
+        JLabel l2 = new JLabel(i3);
+        l2.setBounds(0, 0, 900, 900);
+        add(l2);
 
-        // Set position and size
-        j1.setBounds(0, 0, 700, 850);
-        add(j1);
-
-
-
-
+        l1 = new JLabel("Please Select Your Transaction");
+        l1.setForeground(Color.WHITE);
+        l1.setFont(new Font("System", Font.BOLD, 16));
 
 
+        b1 = new JButton("DEPOSIT");
+        b2 = new JButton("CASH WITHDRAWL");
+        b3 = new JButton("FAST CASH");
+        b4 = new JButton("MINI STATEMENT");
+        b5 = new JButton("PIN CHANGE");
+        b6 = new JButton("BALANCE ENQUIRY");
+        b7 = new JButton("EXIT");
 
-        // Layout and frame settings
-        setLayout(null);  // Use absolute positioning
-        setLocation(300, 0);
-        setSize(700, 900);
+        setLayout(null);
 
+        l1.setBounds(235,300,700,35);
+        l2.add(l1);
+
+        b1.setBounds(190,350,130,35);
+        l2.add(b1);
+
+        b2.setBounds(380,350,130,35);
+        l2.add(b2);
+
+        b3.setBounds(190,400,130,35);
+        l2.add(b3);
+
+        b4.setBounds(380,400,130,35);
+        l2.add(b4);
+
+        b5.setBounds(190,450,130,35);
+        l2.add(b5);
+
+        b6.setBounds(370,450,140,35);
+        l2.add(b6);
+
+        b7.setBounds(300,500,150,35);
+        l2.add(b7);
+
+
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
+        b5.addActionListener(this);
+        b6.addActionListener(this);
+        b7.addActionListener(this);
+
+        setLayout(null);
+        setSize(900,1080);
+        setLocation(100,0);
+        setUndecorated(true);
+        getContentPane().setBackground(Color.GRAY);
         setVisible(true);
+
+
+
+
+
     }
 
-    public static void main(String[] args) {
+    public void actionPerformed(ActionEvent ae){
+
+       if(ae.getSource()==b7){
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args){
         new Transaction();
     }
 }
-
-
-
-
-
